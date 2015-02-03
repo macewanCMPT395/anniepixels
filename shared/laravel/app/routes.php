9418e8414cfeb30a2691b0785815009e8b7adec4
Annie Pixeltrik <lohnerb3@gmail.com>
2015-02-02 20:32:04 -0700

    page for users
    http://localhost:8000/users  shows all users in database as an array

--- shared/laravel/app/routes.php
+++ shared/laravel/app/routes.php
@@ -5,11 +5,6 @@ Route::get('/about', 'PagesController@about');
 
 Route::get('/users', function()
 {
-  $user = new User;
-  $user->username = 'harvardGuy';
-  $user->email = 'mahemail2@aplace.com';
-  $user->password = Hash::make('password');
-  $user->save();
-  return DB::table('users')->get();
-  
+  $users = User::all();
+  return View::make('users/index', ['users' => $users]);
 });
---
+++ shared/laravel/app/views/users/index.blade.php
@@ -0,0 +1,9 @@
+<?doctype>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>All Users</h1>
+    {{ dd($users->toArray()) }}
+  </body>
+</html>
